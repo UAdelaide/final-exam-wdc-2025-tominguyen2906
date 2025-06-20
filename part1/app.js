46ff4492a5152c2b948b8937faaa3d2549b708a3
Tomi <tominguyen2906@gmail.com>
2025-06-20 10:55:33 +0000

    [automatic commit] 2025-06-20 10:55:33 UTC 220.239.194.181

--- part1/app.js
+++ part1/app.js
@@ -22,33 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-let db;
-
-(async () => {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
-    await connection.end();
-
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'testdb'
-    });
-
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
-})();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
