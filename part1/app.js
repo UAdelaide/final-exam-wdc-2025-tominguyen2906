d1da00497f49dc677b4a7cb3fc27ba0a01b49947
Tomi <tominguyen2906@gmail.com>
2025-06-20 10:48:01 +0000

    [automatic commit] 2025-06-20 10:48:01 UTC 220.239.194.181

--- part1/app.js
+++ part1/app.js
@@ -47,4 +47,13 @@ app.get('/api/dogs', function(req, res, next) {
   });
 });
 
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [books] = await db.execute('SELECT * FROM books');
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
+});
+
 module.exports = app;
