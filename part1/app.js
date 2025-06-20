73447d048ac2c32ed2da1537ee9ca490b58837f7
Tomi <tominguyen2906@gmail.com>
2025-06-20 10:49:23 +0000

    [automatic commit] 2025-06-20 10:49:23 UTC 220.239.194.181

--- part1/app.js
+++ part1/app.js
@@ -9,14 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-let db;
-db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'testdb'
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
