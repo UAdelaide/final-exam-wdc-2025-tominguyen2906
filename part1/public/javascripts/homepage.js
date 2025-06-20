c7b487a8e38c81624de5a1266a6c6d051fee5bbc
Tomi <tominguyen2906@gmail.com>
2025-06-20 13:25:55 +0000

    [automatic commit] 2025-06-20 13:25:55 UTC 220.239.194.181

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -1,9 +1,3 @@
 const pictureContainer = document.querySelector('.picture-container');
 fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-        const img = document.createElement('img');
-        img.src = data.message;
-        pictureContainer.appendChild(img);
-    })
-    .catch(error => console.error('Error:', error));
+    
