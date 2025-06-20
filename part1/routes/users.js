c2177accb69cb8720b11f1d6d8a3c628657aaa05
Tomi <tominguyen2906@gmail.com>
2025-06-20 11:01:37 +0000

    [automatic commit] 2025-06-20 11:01:37 UTC 220.239.194.181

--- part1/routes/users.js
+++ part1/routes/users.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/api/dogs', function(req, res, next) {
-  res.send("hello");
-});
-
 module.exports = router;
