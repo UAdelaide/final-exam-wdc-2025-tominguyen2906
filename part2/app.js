066d35e6541cb32686eea4458877493247ffc3f3
Tomi <tominguyen2906@gmail.com>
2025-06-20 16:37:40 +0000

    [automatic commit] 2025-06-20 16:37:39 UTC 220.239.194.181

--- part2/app.js
+++ part2/app.js
@@ -15,7 +15,5 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.post
-
 // Export the app instead of listening here
 module.exports = app;
