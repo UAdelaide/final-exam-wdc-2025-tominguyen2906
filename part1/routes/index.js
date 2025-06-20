a3ba751b6a3936499afefb786911d60fae028f9f
Tomi <tominguyen2906@gmail.com>
2025-06-20 10:44:56 +0000

    [automatic commit] 2025-06-20 10:44:56 UTC 220.239.194.181

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/dogs', function(req, res, next) {
-  const dogs = 'SELECT dog_name, size, '
+  const dogs = 'SELECT dog_name, size, owner_username FROM Dogs';
 });
 
 module.exports = router;
