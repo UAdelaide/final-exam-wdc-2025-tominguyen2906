10428d71f6faa93eb2e7599cf5d7dbc230878b97
Tomi <tominguyen2906@gmail.com>
2025-06-20 17:50:21 +0000

    [automatic commit] 2025-06-20 17:50:21 UTC 220.239.194.181

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,4 +59,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// POST logout
+router.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.json({ message: 'Logout successful' });
+  });
+});
+
 module.exports = router;
