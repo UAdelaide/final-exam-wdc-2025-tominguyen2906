06f45f5823b8aad973ff34b803a5d8ae6779222b
Tomi <tominguyen2906@gmail.com>
2025-06-20 11:45:33 +0000

    [automatic commit] 2025-06-20 11:45:33 UTC 220.239.194.181

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -69,6 +69,8 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Buddy', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Luna', 'medium');
 
+INSERT INTO WalkRatings ()
+
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
