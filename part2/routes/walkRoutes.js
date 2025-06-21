0d9d5609647241ef05a3d789b98e3eecbd3b5472
Tomi <tominguyen2906@gmail.com>
2025-06-21 01:59:40 +0000

    [automatic commit] 2025-06-21 01:59:40 UTC 220.239.195.0

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -19,7 +19,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-
 // POST a new walk request (from owner)
 router.post('/', async (req, res) => {
   const { dog_id, requested_time, duration_minutes, location } = req.body;
